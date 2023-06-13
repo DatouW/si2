fdd36ed5ceff062a4576c9b1e454dfd1e2a3b7c0
datou <datou@gmail.com>
2023-06-13 08:18:53 -0400

    sprint 2

--- si_avicola/src/index.js
+++ si_avicola/src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ConfigProvider } from "antd";
 import {
-  // BrowserRouter,
-  HashRouter,
+  BrowserRouter,
+  // HashRouter,
 } from "react-router-dom";
 import es_ES from "antd/locale/es_ES";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <ConfigProvider locale={es_ES}>
       <App />
     </ConfigProvider>
-  </HashRouter>
+  </BrowserRouter>
 );
